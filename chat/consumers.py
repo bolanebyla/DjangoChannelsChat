06f4e6fe4a4615ff5f89e692093a730ddd1835d9
Алексей <alexleskov2000@gmail.com>
2021-04-20 10:39:24 +0800

    Вывод пользователя

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,6 +10,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        print(self.room_name)
+        print(self.room_group_name)
 
         if self.scope['user'] == AnonymousUser():
             raise DenyConnection("Такого пользователя не существует")
@@ -36,21 +38,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = str(self.scope['user'])
+        user = {
+            'username': username
+        }
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': user,
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        user = event['user']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'user': user
         }))
