c8cbaea849f0badbdf7ca6ce817efe3bb8178bff
Алексей <alexleskov2000@gmail.com>
2021-04-20 10:24:43 +0800

    Подключение к чату только авторизированных пользователей

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,8 +11,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # if self.scope['user'] == AnonymousUser():
-        #     raise DenyConnection("Такого пользователя не существует")
+        if self.scope['user'] == AnonymousUser():
+            raise DenyConnection("Такого пользователя не существует")
 
         user = self.scope['user']
         print(user)
