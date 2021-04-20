d359f22074fb1050b56e68b5b2a5acd16c82ae9d
Алексей <alexleskov2000@gmail.com>
2021-04-20 11:32:31 +0800

    Модель для чатов и сообщений

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'creation_date', 'update_date')
+    search_fields = ('title',)
+    ordering = ['-update_date']
+
+
+@admin.register(models.Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'chat', 'sender', 'creation_date', 'update_date')
+    search_fields = ('text',)
+    ordering = ['-creation_date']
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    clients = models.ManyToManyField(User, blank=True, verbose_name='Пользователи')
+
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        verbose_name = 'Чат'
+        verbose_name_plural = 'Чаты'
+        ordering = ['-update_date']
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Message(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    chat = models.ForeignKey('chat.Chat', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Отправитель')
+
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return f'{self.text}'
